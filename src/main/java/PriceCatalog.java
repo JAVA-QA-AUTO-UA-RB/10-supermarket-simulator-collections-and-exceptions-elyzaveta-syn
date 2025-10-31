12f9b9730fda2c5552692fa88c38df6ba8e7d4ff
elyzaveta-syn <vam.fox86@gmail.com>
2025-10-31 23:25:05 +0100

    added class NoSuchProductException; added  Map<String, Double> catalog, private boolean isHere; added methods addPrice, getPrice, getPrices

---
+++ src/main/java/PriceCatalog.java
@@ -0,0 +1,37 @@
+package main.java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class NoSuchProductException extends RuntimeException {
+    public NoSuchProductException(String product) {
+        super("Товар \"" + product + "\" не існує.");
+    }
+}
+
+public class PriceCatalog {
+
+    Map<String, Double> catalog = new HashMap<>();
+
+    private boolean isHere(String product) {
+        return true;
+    }
+
+    public void addPrice(String product, double price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("Ціна не може бути негативною.");
+        }
+        catalog.put(product, price);
+    }
+
+    public Double getPrice(String product) {
+        if (!isHere(product)) {
+            throw new NoSuchProductException(product);
+        }
+        return catalog.get(product);
+    }
+
+    public HashMap<Object, Double> getPrices() {
+        return new HashMap<>();
+    }
+}
